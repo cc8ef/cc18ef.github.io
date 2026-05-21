e200be9e812203e381082b672d11018b1f5e71ff
Cecilia Clucas <ceciliaeaclucas@gmail.com>
2026-05-21 10:36:32 -0400

    rr3

---
+++ script.js
@@ -0,0 +1,12 @@
+// document.querySelector("p").innerHTML = "XXXXXXXXXXXXXXXX"
+
+// document.querySelector("#p2").innerHTML = "XXXXXXXXXXXXXXXX"
+
+function FirstFunction(){
+    document.querySelector("#p2").innerHTML = "XXXXXXXXXXXXXXXX"}
+
+// FirstFunction()
+
+document.querySelector("#p2").addEventListener("click", FirstFunction)
+
+// FirstFunction()
